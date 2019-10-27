3f5968fd5571f079f0280c6f6e8723c433bc3ff0
Scott W Harden <swharden@gmail.com>
2019-10-27 13:22:39 -0400

    generate HTML report when tests are run

--- src/QuickPlotTests/Unit/Setup.cs
+++ src/QuickPlotTests/Unit/Setup.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Text;
 
 namespace QuickPlotTests.Unit
 {
@@ -21,6 +22,29 @@ namespace QuickPlotTests.Unit
         public void WrapUp()
         {
             // todo: generate markdown and HTML reports
+            GenerateHTML();
+        }
+
+        public void GenerateHTML()
+        {
+            StringBuilder sb = new StringBuilder("<h1>Test Output</h1>\n");
+
+            string[] imagePaths = System.IO.Directory.GetFiles(Tools.outputFolder, "*.png");
+            foreach (string path in imagePaths)
+            {
+                string basename = System.IO.Path.GetFileName(path);
+                sb.AppendLine($"<h2><br><br>{System.IO.Path.GetFileNameWithoutExtension(basename)}</h2>");
+                sb.AppendLine($"<img src='{basename}'>");
+            }
+
+            sb.Insert(0, "<body style='background-color: #EEE; margin: 30px;'>");
+            sb.AppendLine("</body>");
+
+            sb.Insert(0, "<html>");
+            sb.AppendLine("</html>");
+
+            string htmlFilePath = System.IO.Path.Join(Tools.outputFolder, "testResults.html");
+            System.IO.File.WriteAllText(htmlFilePath, sb.ToString());
         }
     }
 }
